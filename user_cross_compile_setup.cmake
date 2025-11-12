7b0ca551b002d3c64ef90679d4830a25cd50aa2f
student <student@embedded>
2025-11-12 20:36:59 +0800

    完成了交叉编译，demo可以在开发板上运行 //TODO：缺少处理输入的库，需要解决后重新编译，在开发板上实现输入输出全功能的LVGL库

--- user_cross_compile_setup.cmake
+++ user_cross_compile_setup.cmake
@@ -5,9 +5,10 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-set(tools /home/ubuntu/Your_SDK/prebuilt/rootfsbuilt/arm/toolchain-glibc-gcc/toolchain)
-set(CMAKE_C_COMPILER ${tools}/bin/arm-openwrt-linux-gnueabi-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/arm-openwrt-linux-gnueabi-g++)
+set(tools /opt/aarch64-buildroot-linux-gnu_sdk-buildroot)
+set(CMAKE_C_COMPILER ${tools}/bin/aarch64-linux-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-linux-g++)
+set(SYSROOT ${tools}/aarch64-buildroot-linux-gnu/sysroot)
 
 # If necessary, set STAGING_DIR
 # if not work, please try(in shell command): export STAGING_DIR=/home/ubuntu/Your_SDK/out/xxx/openwrt/staging_dir/target
